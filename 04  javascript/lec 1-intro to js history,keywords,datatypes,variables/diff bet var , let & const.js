a4037120641558ae2adeb6e70a0809086871d94c
preetysahu3560-cmyk <preetysahu3560@gmail.com>
2026-07-21 07:30:28 +0530

    diff bet var,let & const

---
+++ 04  javascript/lec 1-intro to js history,keywords,datatypes,variables/diff bet var , let & const.js
@@ -0,0 +1,43 @@
+//function myFun() {
+  //let var1 = 10;
+  //const var2 = 20;
+  //var var3 = 30
+//}
+
+//myFun()
+
+// console.log(var1)
+// console.log(var2)
+// console.log(var3)
+//=========DT - 20 7 26
+//{
+//let a = 10;
+//const b =20;
+//var c =30
+//}
+//function fun(){
+// debugger
+//let a1= 10;
+//const b1 =20;
+//var c1 =30
+//console.log("hiiiiii");
+
+//}
+//fun()
+//ii)
+//const name ="Priti"
+//console.log("Before reassign",name)
+//const name = "Pritismita"
+//console.log("After reassign",name)
+//iii)
+//var instituteName = "JT"
+//console.log("instituteName------",instituteName)
+//var instituteName = "JAVA TECHNOCRAT"
+//console.log("instituteName===== ", instituteName)
+//iv)
+debugger
+console.log(x)
+console.log(y)
+var x = 10
+let y =20;
+const z = 30
