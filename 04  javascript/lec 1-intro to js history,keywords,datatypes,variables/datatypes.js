cf35767e6593b9db792b9cce0f9a498b8904c79b
preetysahu3560-cmyk <preetysahu3560@gmail.com>
2026-07-21 07:27:29 +0530

    datatypes

---
+++ 04  javascript/lec 1-intro to js history,keywords,datatypes,variables/datatypes.js
@@ -0,0 +1,26 @@
+console.log(typeof 10)
+console.log(typeof 10.2)
+console.log(typeof NaN)
+
+console.log(typeof "10")
+console.log(typeof "hello")
+
+console.log(typeof true)
+console.log(typeof false)
+
+console.log(typeof undefined)
+
+console.log(typeof null)
+
+console.log(typeof Symbol(10))
+
+console.log(typeof BibInt(23))
+
+let a ;
+console.log(a)
+
+console.log(b)
+var b = 20
+
+let h4 = document.querySelector("h4")
+console.log(h4)
