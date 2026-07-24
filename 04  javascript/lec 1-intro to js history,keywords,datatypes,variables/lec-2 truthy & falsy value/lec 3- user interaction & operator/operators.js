779facd88227a837b9347f3f6d8a5f152daef35e
preetysahu3560-cmyk <preetysahu3560@gmail.com>
2026-07-24 08:31:13 +0530

    user interaction and operators

---
+++ 04  javascript/lec 1-intro to js history,keywords,datatypes,variables/lec-2 truthy & falsy value/lec 3- user interaction & operator/operators.js
@@ -0,0 +1,24 @@
+//console.log(2 ** 3)
+//console.log(4 ** 3)
+//let x=10
+//x **= 2 // x = x**2
+//console.log(x)
+//console.log(5 == "5")
+//nsole.log(5 == "4")
+//console.log("")
+//console.log(1 == true)
+//console.log(0 == false)
+//console.log(5 === "5")
+//console.log(1 === true)
+//console.log(0 === false)
+//console.log(0 != false)
+//console.log(5 != 3)
+//console.log(4 << 3) // 4 * 2^3 //32
+//console.log(8 << 5)// 8 * 2^5//256
+//console.log(64 >> 5) //64 / 2^5 //2
+//console.log(256 >> 3)// 256 / 2^3 //32
+//>>> (unsigned right shift)
+//console.log(256 >>> 3)// 256 / 2^3 //32
+//console.log(64 >>> 3)// 64 / 2^3 //8 
+let x = 5 , y = 20
+console.log(x >y ? x : y)
---
+++ 04  javascript/lec 1-intro to js history,keywords,datatypes,variables/lec-2 truthy & falsy value/lec 3- user interaction & operator/user interaction.js
@@ -0,0 +1,18 @@
+//alert("Hello Everyone!")
+//window.alert("Good Morning")
+//let name = prompt("Enter your name:-")
+//console.log(name)
+
+//if(name){
+  //  console.log("your name is",name)
+//}
+//else{
+   // console.log("re enter user name")
+  // console.log("name is",prompt("Re enter user name"))
+//}
+//let choice = confirm("Are you enjoying js classes ")
+//console.log(choice)
+console.log("Hii") //normal
+console.warn("Hello");//yellow
+console.error("Bye");//red
+
